f399eaa06431e97cf17a56f4f1c45acc108195b6
Jackie Loerincs <jloerinc@mymai.mines.edu>
2016-11-08 16:58:26 -0700

    Added File menu bar with functionality

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,8 +1,13 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class ClueGame {
 	public static Board board;
@@ -20,10 +25,42 @@ public class ClueGame {
 		board.initialize();
 		board.deal();
 		
+		//File Menu
+		JMenu menu = new JMenu("File");
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(menu);
+		
+		createMenuLayout(menu);
+		
+		frame.setJMenuBar(menuBar);
+		
 		frame.add(board,BorderLayout.CENTER);
 		frame.setVisible(true);
-		
+	}
+	
+	public static void createMenuLayout(JMenu menu){
+		// Create notes
+		JMenuItem notes = new JMenuItem("Show Notes");
 		DetectiveNotesGUI gui = new DetectiveNotesGUI(board);
-		gui.setVisible(true);
+		
+		class NotesListener implements ActionListener {
+			public void actionPerformed(ActionEvent e){
+				gui.setVisible(true);
+			}
+		}
+		notes.addActionListener(new NotesListener());
+		
+		// Create exit
+		JMenuItem exit = new JMenuItem("Exit");
+		class ExitListener implements ActionListener {
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}
+		}
+		exit.addActionListener(new ExitListener());
+		
+		menu.add(notes);
+		menu.add(exit);
 	}
 }
