b981c7c8fb23afc1b38f3f1d9ba314661b3a0b59
Jackie Loerincs <jloerinc@mymai.mines.edu>
2016-11-13 11:38:07 -0700

    Added NextPlayerListener class to ClueGame.

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,18 +1,29 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class ClueGame {
 	public static Board board;
 	private static boolean gameOver = false;
+	private static boolean humanFinished;
+	private static int playerIndex = 0;
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Clue Game Board");
@@ -26,6 +37,56 @@ public class ClueGame {
 		board.setWPConfigFiles("data/ENTM_CluePlayers.txt", "data/ENTM_ClueWeapons.txt");
 		board.initialize();
 		board.deal();
+		
+		// Mouse click cycles through players
+		class NextPlayerListener implements MouseListener{
+
+			@Override
+			public void mouseClicked(MouseEvent event) {
+				if (playerIndex == 0){
+					humanFinished = false;
+					humanNext(board);
+				}
+				else
+				{
+					computerNext(board);
+				}
+				
+				//Increment player index
+				if(playerIndex == 5){
+					playerIndex = 0;
+				}
+				else{
+					playerIndex++;	
+				}
+				
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		}
 		
 		//File Menu
 		JMenu menu = new JMenu("File");
@@ -41,8 +102,57 @@ public class ClueGame {
 		frame.setVisible(true);
 		
 		//Add control panel
-		ControlPanelGUI controlGui = new ControlPanelGUI();
-		frame.add(controlGui, BorderLayout.SOUTH);
+		JPanel masterPanel = new JPanel();
+		masterPanel.setLayout(new GridLayout(2,3));
+
+		//WhoseTurn
+		JPanel whoseTurn = new JPanel();
+		whoseTurn.setLayout(new GridLayout(3,1));
+		JLabel turnLabel = new JLabel("Whose turn?");
+		JTextField turnText = new  JTextField(50);
+		whoseTurn.add(turnLabel);
+		whoseTurn.add(turnText);
+		
+		//Die
+		JPanel die = new JPanel();
+		JLabel rollLabel = new JLabel("Roll");
+		JTextField rollText = new  JTextField(10);
+		die.add(rollLabel);
+		die.add(rollText);
+		die.setBorder(new TitledBorder (new EtchedBorder(), "Die"));
+		
+		//Guess
+		JPanel guess = new JPanel();
+		//guess.setLayout(new GridLayout(2,1));
+		JLabel guessLabel = new JLabel("Guess");
+		JTextField guessText = new  JTextField(20);
+		guess.add(guessLabel);
+		guess.add(guessText);
+		guess.setBorder(new TitledBorder (new EtchedBorder(), "Guess"));
+		
+		//GuessResult
+		JPanel guessResult = new JPanel();
+		//guessResult.setLayout(new GridLayout(2, 1));
+		JLabel resultLabel = new JLabel("Response");
+		JTextField resultText = new  JTextField(20);
+		guessResult.add(resultLabel);
+		guessResult.add(resultText);
+		guessResult.setBorder(new TitledBorder (new EtchedBorder(), "Guess Result"));
+
+		
+		//add buttons to the button panel
+		JButton nextPlayerButton = new JButton("Next player");
+		nextPlayerButton.addMouseListener(new NextPlayerListener());
+		masterPanel.add(nextPlayerButton);
+		JButton accusationButton = new JButton("Make an accusation");
+		masterPanel.add(accusationButton);
+		
+		//add Labels to the button Panel
+		masterPanel.add(whoseTurn);
+		masterPanel.add(die);
+		masterPanel.add(guess);
+		masterPanel.add(guessResult);
+		frame.add(masterPanel, BorderLayout.SOUTH);
 		
 		
 		//Add shown cards
@@ -53,15 +163,15 @@ public class ClueGame {
 		JOptionPane splash = new JOptionPane();
 		splash.showMessageDialog(splash, "You are " + board.player.getName() + "! Press NextPlayer to begin playing!");
 		
-		//while game not over
+		
+		
+		/*while game not over
 		while (gameOver == false) {
-			class NextPlayerListener implements ActionListener {
-				public void actionPerformed(ActionEvent e){
-					//do next player code (FUNCTION in ClueGame)-Pass in next player
-					//go to next player
-				}
-			}
-		}
+			System.out.println();			
+		}*/
+		
+		
+		
 	}
 	
 	public static void createMenuLayout(JMenu menu){
@@ -88,4 +198,15 @@ public class ClueGame {
 		menu.add(notes);
 		menu.add(exit);
 	}
+	
+	public static void humanNext(Board board){
+		System.out.println("Human");
+	}
+	
+	public static void computerNext(Board board){
+		System.out.println("Computer");
+	}
+	
+	
+	
 }
